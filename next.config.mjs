0f3bc7c8d4085923d6f5baf6c7b682464ec57153
folondor <151128633+folondor@users.noreply.github.com>
2026-04-23 14:57:27 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',        
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  basePath: isProd ? '/mtbtest' : '',
+  assetPrefix: isProd ? '/mtbtest/' : '',
   images: {
     unoptimized: true,
   },
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
